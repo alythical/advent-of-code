9f1d03a66465f34af8eaa4800e3d24643634b357
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-08-17 19:51:37 -0500

    fix: warnings in `InputLoader`

--- kotlin/src/main/kotlin/InputLoader.kt
+++ kotlin/src/main/kotlin/InputLoader.kt
@@ -1,11 +1,11 @@
-class InputLoader() {
+class InputLoader {
     private val loader: ClassLoader = Thread.currentThread().contextClassLoader
 
     fun getExample(year: Int, day: Int): String {
-        return this.loader.getResource("$year/examples/day$day.txt").readText()
+        return this.loader.getResource("$year/examples/day$day.txt")!!.readText()
     }
 
     fun get(year: Int, day: Int): String {
-        return this.loader.getResource("$year/actual/day$day.txt").readText()
+        return this.loader.getResource("$year/actual/day$day.txt")!!.readText()
     }
 }
